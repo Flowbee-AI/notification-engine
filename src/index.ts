05ea36dd36ef81bb40579d38b7cf1be9c0c7fc51
khushal <khushalbhasin4488@gmail.com>
2025-03-21 15:51:33 +0530

    init2

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ async function main(){
     let data: QueueJobType = {
         userId: "new_user",
         payload: {
-            title: "Test",
-            description: "Test",
+            title: "title ",
+            description: "description",
             imageUrl: "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             type: "BUILDER",
             link: "https://www.google.com"
